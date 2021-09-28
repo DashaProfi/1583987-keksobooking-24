09844d95b79adfcf26aa66586859cd52168a29c1
Dariia Shaihullina <dariyn@yandex.ru>
2021-09-28 15:04:45 +0500

    добавлены вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+function getRandomIntFromRange(min, max) {
+  let result;
+  if (min >= 0 && max > min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    result = Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    result = 'Введите корректное значение диапазона';
+  }
+  return result;
+}
+getRandomIntFromRange(7, 9);
 
+
+function getRandomGeoCoordinates(min, max, num) {
+  let result;
+  if (min >= 0 && max > min) {
+    // min = Math.ceil(min);
+    // max = Math.floor(max);
+    result = +(Math.random() * (max - min + 1) + min).toFixed(num);
+  } else {
+    result = 'Введите корректное значение диапазона';
+  }
+  return result;
+}
+getRandomGeoCoordinates(1.89, 8.456, 3);
